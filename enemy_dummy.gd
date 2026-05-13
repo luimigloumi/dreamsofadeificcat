99142c356f570c44bdd655e44ccd3c409f4483fc
GlacialSkyfarer <168661337+luimigloumi@users.noreply.github.com>
2026-05-13 12:50:33 -0600

    that yoinky sploinky

--- enemy_dummy.gd
+++ enemy_dummy.gd
@@ -19,6 +19,7 @@ func on_death(_other: Node3D):
 	if death_effect != null:
 		var effect = death_effect.instantiate()
 		get_parent().add_child(effect)
+		effect.global_position = global_position
 	if debug: print("auuuugh")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,4 +27,4 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	velocity.x = move_toward(velocity.x, 0, (1.0 / slipperiness) * delta)
 	velocity.z = move_toward(velocity.z, 0, (1.0 / slipperiness) * delta)
-	pass
+	move_and_slide()
